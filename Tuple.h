f2c2b4832672badd0598d03e60008da748963e5c
Julio <julio@Julios-iMac.local>
2018-10-23 04:25:02 -0700

    WIP attempting to fix alignment/size

--- Tuple.h
+++ Tuple.h
@@ -10,6 +10,16 @@ struct Tuple
         construct(std::make_index_sequence<sizeof...(T)>{}, std::forward<T>(t)...);
     }
 
+    /*
+    template<typename <typename...> class SrcTuple, typename... U>
+    explicit Tuple(SrcTuple<U...>&& src)
+    {
+        static_assert(sizeof...(T) == sizeof...(U), "Size of source tuple must match target's");
+        map(src, std::get<>,
+                [](U&&... u) { construct(std::make_index_sequence<sizeof...(T)>{}, std::forward<U>(u)...);
+    }
+    */
+
     template<size_t Idx>
     decltype(auto) get()
     {
@@ -18,7 +28,7 @@ struct Tuple
 
 private:
     template<size_t Idx, size_t Target, typename Head, typename... Tail>
-    decltype(auto) getRef()
+    decltype(auto) getRef() noexcept
     {
         if constexpr (Idx == Target) {
             return *reinterpret_cast<Head*>(&buffer[offsetAt(Idx)]);
@@ -35,14 +45,21 @@ private:
         (void)dummy;
     }
 
-    static constexpr size_t offsetAt(size_t idx)
+    static constexpr size_t offsetAt(size_t idx) noexcept
     {
         constexpr size_t sizes[] = {
-            sizeof(std::aligned_storage_t<sizeof(T)>)...
+            sizeof(std::aligned_storage_t<sizeof(T), alignof(T)>)...
         };
+        constexpr size_t alignments[] = { alignof(T)... };
+        constexpr size_t count = sizeof...(T);
+
         size_t offset = 0;
-        for (size_t i = 0; i != idx; ++i) {
-            offset += sizes[i];
+        for (size_t i = 1; i <= idx; ++i) {
+            offset += sizes[i-1];
+            if (i < count) {
+                offset += sizes[i] - 1;
+                offset &= ~(alignments[i] - 1);
+            }
         }
         return offset;
     }
--- Tuple_ut.cpp
+++ Tuple_ut.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 #include "Tuple.h"
+#include <tuple>
 
+//------------------------------------------------------------------------------
 TEST(tuple, initialize)
 {
     Tuple t(1, 'a', 1.2);
@@ -14,4 +16,24 @@ TEST(tuple, initialize)
 
     t.get<2>() = 123.45;
     ASSERT_DOUBLE_EQ(123.45, t.get<2>());
+}
+
+//------------------------------------------------------------------------------
+TEST(tuple, alignment)
+{
+    struct A { char a_; int b_; char c_; };
+    struct C{};
+    struct D{char x; double y;};
+    struct E{char a; char b; int c;};
+    struct F{char a; char b; char c; int d; };
+    struct G{char a; char b; char c; char d; int e;};
+
+    Tuple a('a', 123, 'b');
+    ASSERT_EQ(sizeof(A), sizeof(a));
+
+    struct alignas(64) X { int x; };
+
+    using tuple_b = std::tuple<char, X, char>;
+    Tuple b('a', X{123}, 'b');
+    ASSERT_EQ(sizeof(tuple_b), sizeof(b));
 }
